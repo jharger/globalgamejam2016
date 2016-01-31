817860238d339c330b59f01454699b7e10c87690
kurtedwardhollowell@gmail.com <kurtedwardhollowell@gmail.com>
2016-01-31 13:57:44 -0700

    Putting Everything Together

---
+++ Assets/DisableOnTime.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DisableOnTime : MonoBehaviour {
+
+    public float disableTime = 5;
+    private float disableTimer = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+    {
+        disableTimer += Time.deltaTime;
+        if(disableTimer > disableTime)
+        {
+            this.gameObject.SetActive(false);
+        }
+	}
+}
