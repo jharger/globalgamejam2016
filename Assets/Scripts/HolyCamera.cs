44f9720bd2e5c40eb961a0de2247834cad87c76f
John Harger <john.harger@gmail.com>
2016-01-31 13:41:34 -0700

    Added Holy Camera

---
+++ Assets/Scripts/HolyCamera.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HolyCamera : MonoBehaviour {
+    public Material mat;
+    public float nearClip = 7f;
+
+    Camera holyCam;
+    RenderTexture tex;
+
+    void Awake()
+    {
+        GameObject holyObject = new GameObject("Holy Camera");
+        holyObject.transform.SetParent(transform, false);
+        Camera mainCam = GetComponent<Camera>();
+        holyCam = holyObject.AddComponent<Camera>();
+        holyCam.CopyFrom(mainCam);
+        holyCam.nearClipPlane = nearClip;
+    }
+
+    void Start()
+    {
+        tex = new RenderTexture(Screen.width, Screen.height, 0);
+        holyCam.targetTexture = tex;
+        mat.SetTexture("_SubTex", tex);
+    }
+
+    void OnRenderImage(RenderTexture src, RenderTexture dest)
+    {
+        Graphics.Blit(src, dest, mat);
+    }
+}
